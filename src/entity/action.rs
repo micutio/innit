5ac7f3160ac94b1ea0c3bb186bfe6755a210d3c9
Michael Wagner <wagner.mchl@googlemail.com>
2019-07-08 18:16:39 +0800

    added: move actions and trait bounds for action trait objects

--- src/entity/action.rs
+++ src/entity/action.rs
@@ -3,8 +3,9 @@
 /// This module provides the action interface, which is used to create any kind
 /// of action that can be performed by the player or an NPC.
 use std::rc::Rc;
+use std::fmt::Debug;
 
-use entity::object::Object;
+use entity::object::{ObjectVec, Object};
 use game_state::GameState;
 
 pub enum ActionResult {
@@ -14,16 +15,19 @@ pub enum ActionResult {
     Failure,
     /// Another action happens automatically after this one.
     Consequence {
-        action: Option<Rc<Action>>,
+        action: Option<Rc<dyn Action>>,
     },
     // Another action happens as the same time as this one.
     SideEffect {
-        action: Option<Rc<Action>>,
+        action: Option<Rc<dyn Action>>,
     },
 }
 
-pub trait Action {
-    fn perform(&self, objects: &mut [Object], game_state: &mut GameState) -> ActionResult;
+#[typetag::serde(tag = "type")]
+pub trait Action: Debug {
+    fn perform(&self, owner: &mut Object, objects: &mut ObjectVec, game_state: &mut GameState) -> ActionResult;
+
+    fn get_energy_cost(&self) -> i32;
 }
 
 // Example action
@@ -31,13 +35,15 @@ pub trait Action {
 pub struct AttackAction {
     base_power: i32,
     target_id: Option<usize>,
+    energy_cost: i32,
 }
 
 impl AttackAction {
-    pub fn new(base_power: i32) -> Self {
+    pub fn new(base_power: i32, energy_cost: i32) -> Self {
         AttackAction {
             base_power,
             target_id: None,
+            energy_cost,
         }
     }
 
@@ -46,15 +52,63 @@ impl AttackAction {
     }
 }
 
+#[typetag::serde]
 impl Action for AttackAction {
-    fn perform(&self, objects: &mut [Object], game_state: &mut GameState) -> ActionResult {
+    fn perform(&self, owner: &mut Object, objects: &mut ObjectVec, game_state: &mut GameState) -> ActionResult {
         match self.target_id {
             Some(target_id) => {
                 // TODO: Replace with defend action.
-                objects[target_id].take_damage(self.base_power, game_state);
+                // unwrap should be safe to use here because the object not available
+                // in `objects` is the owner of this action.
+                objects[target_id].unwrap().take_damage(self.base_power, game_state);
                 ActionResult::Success
             }
             None => ActionResult::Failure,
         }
+    }
+
+    fn get_energy_cost(&self) -> i32 {
+        self.energy_cost
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Direction {
+    North, South, East, West,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MoveAction {
+    direction: Direction,
+    energy_cost: i32,
+}
+
+impl MoveAction {
+    fn new(direction: Direction, energy_cost: i32) -> Self {
+        MoveAction {
+            direction,
+            energy_cost,
+        }
+    }
+}
+
+#[typetag::serde]
+impl Action for MoveAction {
+    fn perform(&self, owner: &mut Object, objects: &mut ObjectVec, game_state: &mut GameState) {
+        let (dx, dy) = match self.direction {
+            North => (0, -1),
+            South => (0, 1),
+            East => (1, 0),
+            West => (-1, 0),
+        };
+
+        let (x, y) = owner.pos();
+        if !is_blocked(game_state.world, objects, x + dx, y + dy) {
+            owner.set_pos(x + dy, y + dy);
+        }
+    }
+
+    fn get_energy_cost(&self) -> i32 {
+        self.energy_cost
     }
 }
