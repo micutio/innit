ea16214b20ca995f51959dd19cbe60506af30eb9
Michael Wagner <wagner.mchl@googlemail.com>
2019-07-24 00:26:20 +0800

    added: documentation and first outline

--- src/entity/dna.rs
+++ src/entity/dna.rs
@@ -1,27 +1,28 @@
 /// Module DNA
 ///
-/// The DNA contains all core information, excluding temporary info such as
-/// position etc. This module allows to generate objects from DNA and modify
-/// them using mutation as well as crossing.
-/// Decoding DNA delivers attributes and functions that fall into one of three
-/// categories: perception, orientation (a.k.a. processing), actuation.
+/// The DNA contains all core information, excluding temporary info such as position etc. This
+/// module allows to generate objects from DNA and modify them using mutation as well as crossing.
+/// Decoding DNA delivers attributes and functions that fall into one of three categories:
+/// perception, orientation (a.k.a. processing), actuation.
 ///
-use entity::object::Object;
-
-pub fn generate_dna(object: &Object) -> String {
-    unimplemented!();
-}
-
-pub fn parse_dna_to_object(dna: &str) -> Object {
-    unimplemented!();
-}
-
-enum ParsingState {
-    Init,
-    ReadAttribute,
-    ReadSkill,
-}
-
-struct DnaParser {
-    state: ParsingState,
+/// A DNA Genome is implemented as a string of hexadecimal numbers. The start of a gene is marked
+/// by the number zero. Genes can overlap, so that parsing the new gene resumes "in the middle" of
+/// a previous gene. The genes should be small and encoding the presence of a quality. Versatility
+/// is then controlled by the cumulative occurrence of a gene.
+/// Basically: the more often a gene occurs, the stronger it's trait will be.
+///
+/// List of potential genes:
+///
+/// | Gene | Primary Trait | Trait Attributes | Potential Syngery    | Potential Anti-Synergy |
+/// | ---- | ------------- | ---------------- | -------------------- | ---------------------- |
+/// |      | sensing       | range, accuracy  | movement (organelle) | camouflage?            |
+/// |      | movement      | speed, direction | sensing (organelle)  | camouflage?            |
+/// |      | attack        | energy, damage   |                      | defense                |
+/// |      | defense       | membrane         |                      | attack                 |
+/// |      | camouflage    | energy, accuracy |                      |                        |
+///
+// TODO: Design a DNA parser and a mapping from symbol to trait struct.
+// TODO: Can behavior be encoded in the genome too i.e., fight or flight?
+struct DNA {
+    sequence: String,
 }
