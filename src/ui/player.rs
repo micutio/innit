749464f3eb4a9e596b5e4fb0c8a54e664d17dc0e
Michael Wagner <wagner.mchl@googlemail.com>
2019-07-31 16:19:46 +0800

    added: separate player-specific settings file

---
+++ src/ui/player.rs
@@ -0,0 +1,4 @@
+//! Game settings pertaining to the player.
+//! This defines player actions, key mappings and more.
+
+pub const PLAYER: usize = 0; // player object reference, index of the object vector
