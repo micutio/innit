9f7d77fe4585113ea1f17a1ba212e306ae3dd4e5
Michael Wagner <wagner.mchl@googlemail.com>
2019-08-06 01:10:23 +0800

    changed: started implementation of seedable, serializable rng

--- src/util/game_rng.rs
+++ src/util/game_rng.rs
@@ -1,17 +1,31 @@
-//! A game-global random number generator
-//! For more info on Rust RNGs, refer to https://rust-random.github.io/book/guide-rngs.html
-//! For infor on serializable RNG, refer to
-//!     https://github.com/rsaarelm/calx/blob/45a8d78edda35f2acd59bf9d2bf96fbb98b214ed/calx-alg/src/rng.rs#L33-L84
-
 use rand::{Rng, SeedableRng};
-use rand_pcg::Pcg32;
 
-// #[derive(Serialize, Deserialize)]
-pub struct GameRng(Pcg32);
+/// A seedable random number generator that can be serialized for consistent random number
+/// generation. For more info on Rust RNGs, refer to https://rust-random.github.io/book/guide-rngs.html
+/// For infor on serializable RNG, refer to
+///     https://github.com/rsaarelm/calx/blob/45a8d78edda35f2acd59bf9d2bf96fbb98b214ed/calx-alg/src/rng.rs#L33-L84
+#[derive(Clone, Debug)]
+pub struct GameRng<T> {
+    inner: T,
+}
 
-impl GameRng {
-    pub fn seed_game_rng(seed: [u8; 16]) -> Self {
-        GameRng(Pcg32::from_seed(seed))
+impl<T: Rng + 'static> GameRng<T> {
+    pub fn new(inner: T) -> GameRng<T> {
+        GameRng { inner }
     }
 }
 
+impl<T: SeedableRng + Rng + 'static> SeedableRng for GameRng<T> {
+
+    // fn reseed(&mut self, seed: S) {
+    //     self.inner.reseed(seed);
+    // }
+
+    // For implementing seed refer to: https://rust-random.github.io/rand/rand_core/trait.SeedableRng.html
+    // type Seed: Sized + Default + asMut<[u8]>;
+
+
+    fn from_seed(seed: <T as rand::SeedableRng>::Seed) -> GameRng<T> {
+        GameRng::new(SeedableRng::from_seed(seed))
+    }
+}
