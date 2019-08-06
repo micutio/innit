4e10bd40e2a66a02acee0e163e8114bf0d808095
Michael Wagner <wagner.mchl@googlemail.com>
2019-08-06 17:45:44 +0800

    added: rng seed implementation

--- src/util/game_rng.rs
+++ src/util/game_rng.rs
@@ -1,5 +1,21 @@
 use rand::{Rng, SeedableRng};
 
+
+const N: usize = 64;
+pub struct GameRngSeed(pub [u8; N]);
+
+impl Default for GameRngSeed {
+    fn default() -> GameRngSeed {
+        GameRngSeed([0; N])
+    }
+}
+
+impl AsMut<[u8]> for GameRngSeed {
+    fn as_mut(&mut self) -> &mut [u8] {
+        &mut self.0
+    }
+}
+
 /// A seedable random number generator that can be serialized for consistent random number
 /// generation. For more info on Rust RNGs, refer to https://rust-random.github.io/book/guide-rngs.html
 /// For infor on serializable RNG, refer to
@@ -8,6 +24,7 @@ use rand::{Rng, SeedableRng};
 pub struct GameRng<T> {
     inner: T,
 }
+
 
 impl<T: Rng + 'static> GameRng<T> {
     pub fn new(inner: T) -> GameRng<T> {
@@ -22,10 +39,12 @@ impl<T: SeedableRng + Rng + 'static> SeedableRng for GameRng<T> {
     // }
 
     // For implementing seed refer to: https://rust-random.github.io/rand/rand_core/trait.SeedableRng.html
-    // type Seed: Sized + Default + asMut<[u8]>;
+    // type Seed = dyn Default + Sized + AsMut<[u8]>;
+    // type Seed = [u8; 64];
+    type Seed = GameRngSeed;
 
-
-    fn from_seed(seed: <T as rand::SeedableRng>::Seed) -> GameRng<T> {
+    // fn from_seed(seed: <T as rand::SeedableRng>::Seed) -> GameRng<T> {
+    fn from_seed(seed: GameRngSeed) -> GameRng<T> {
         GameRng::new(SeedableRng::from_seed(seed))
     }
 }
