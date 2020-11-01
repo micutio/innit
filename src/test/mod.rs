7f79a491ce26665d6ce098ba456a4ed64b1ec062
Micutio <wagner.mchl@googlemail.com>
2020-11-01 23:50:44 +0800

    added: unit test for position functions

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,3 +1,4 @@
 mod ai;
 #[cfg(test)]
 mod genetics;
+mod position;
---
+++ src/test/position.rs
@@ -0,0 +1,33 @@
+#[test]
+fn test_adjacent() {
+    use crate::entity::object::Position;
+
+    let pos_1 = Position::new(10, 10);
+    let pos_2 = Position::new(10, 10);
+    let pos_3 = Position::new(10, 9);
+    let pos_4 = Position::new(10, 11);
+    let pos_5 = Position::new(9, 10);
+    let pos_6 = Position::new(11, 10);
+    let pos_7 = Position::new(11, 11);
+    let pos_8 = Position::new(9, 9);
+    let pos_9 = Position::new(11, 9);
+    let pos_10 = Position::new(9, 11);
+    let pos_11 = Position::new(10, 8);
+    let pos_12 = Position::new(10, 12);
+    let pos_13 = Position::new(8, 10);
+    let pos_14 = Position::new(12, 10);
+
+    assert!(!pos_1.is_adjacent(&pos_2));
+    assert!(pos_1.is_adjacent(&pos_3));
+    assert!(pos_1.is_adjacent(&pos_4));
+    assert!(pos_1.is_adjacent(&pos_5));
+    assert!(pos_1.is_adjacent(&pos_6));
+    assert!(!pos_1.is_adjacent(&pos_7));
+    assert!(!pos_1.is_adjacent(&pos_8));
+    assert!(!pos_1.is_adjacent(&pos_9));
+    assert!(!pos_1.is_adjacent(&pos_10));
+    assert!(!pos_1.is_adjacent(&pos_11));
+    assert!(!pos_1.is_adjacent(&pos_12));
+    assert!(!pos_1.is_adjacent(&pos_13));
+    assert!(!pos_1.is_adjacent(&pos_14));
+}
