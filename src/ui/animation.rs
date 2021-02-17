b1bd3707667de9b22988de40800b19b0db8aee71
Micutio <wagner.mchl@googlemail.com>
2021-02-17 12:09:55 +0800

    removed animation module, fixed object processing

--- src/ui/animation.rs
+++
@@ -1,8 +0,0 @@
-#[derive(PartialEq, Debug)]
-pub enum AnimationType {
-    /// Gradual transition of the world hue and or brightness
-    ColorTransition,
-    /// A cell flashes with a specific character.
-    /// Example: flash a red 'x' over an object to indicate a hit.
-    FlashEffect,
-}
